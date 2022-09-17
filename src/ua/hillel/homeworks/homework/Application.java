1b240af5df3630310856f5edd7c5304073c4fdc1
LucyLenn <ov.lclnn@gmail.com>
2022-09-17 11:52:43 +0300

    lesson-15: Added project for homework 13

--- src/ua/hillel/homeworks/homework/Application.java
+++ src/ua/hillel/homeworks/homework/Application.java
@@ -8,7 +8,6 @@ import java.util.function.Supplier;
 
 public class Application {
     public static void main(String[] args) {
-
         System.out.println(isStringNonEmpty(""));
         System.out.println(isStringNonEmpty("testString"));
 
